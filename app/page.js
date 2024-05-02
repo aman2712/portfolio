eca17fba5b63ce2c35ddcd4db46bb51a71faf01e
aman2712 <khanamankhan1973@gmail.com>
2024-05-02 23:05:39 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center h-screen flex-col">
+      <p className="tracking-widest text-3xl mb-5">MD AMAN KHAN</p>
+    </main>
+  );
+}
